3abf9679b45bef0ebc8d855966a3fff3ceee7644
gooking <javazj@qq.com>
2019-02-01 08:32:56 +0800

    更新接口

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# wxapi
+# wxapi - 微信小程序开发接口工具包
 微信小程序接口工具包，无需服务器，无需开发后台，开箱即用，轻松开发小程序
+
+交流QQ群: 629639122
+
+欢迎大家进群交流
+
+## 使用方法
+1、将本项目文件夹 “wxapi” 复制到您的小程序根目录；
+
+2、 修改 config.js 文件， subDomain 改为你自己的域名
+> 你可以 [免费开通后台账号](https://www.it120.cc/) ，开通后查看您的 subDomain 「[如何查看专属subDomain](https://www.it120.cc/info/faq/10468)」
+
+3、登录你的[小程序后台](https://mp.weixin.qq.com)，[设置小程序合法服务器域名](https://www.it120.cc/info/faq/10469)，修改后需要重启小程序开发工具才能生效
+
+4、在您需要的地方引用工具类
+> const WXAPI = require('wxapi/main')  
+> 
+> 或者
+> 
+> import WXAPI from 'wxapi/main'
+
+5、根据下面的文档调用接口即可，如：
+```java
+WXAPI.queryMobileLocation('13500000000').then(res => {
+    console.log('接口成功返回:', res)
+}).catch(e => {
+    console.error('接口调用异常:', e)
+})
+```
+## 接口文档
+### 免费接口
--- wxapi/main.js
+++ wxapi/main.js
@@ -1,8 +1,9 @@
+// 小程序开发api接口工具包，https://github.com/gooking/wxapi
 const CONFIG = require('config.js')
 const API_BASE_URL = 'https://api.it120.cc'
 
 const request = (url, needSubDomain, method, data) => {
-  let _url = API_BASE_URL + (needSubDomain ? CONFIG.subDomain : '') + url
+  let _url = API_BASE_URL + (needSubDomain ? '/' + CONFIG.subDomain : '') + url
   return new Promise((resolve, reject) => {
     wx.request({
       url: _url,
@@ -24,28 +25,36 @@ const request = (url, needSubDomain, method, data) => {
   })
 }
 
-/**
- * 
- {
-  "code": 0,
-  "data": {
-    "areaCode": "0571",
-    "cardType": "GSM",
-    "cityName": "杭州市",
-    "code": 1865712,
-    "id": 199827,
-    "postCode": "310000",
-    "province": "浙江",
-    "segmentName": "中国联通"
-    },
-    "msg": "success"
- }
- */
-const queryMobileLocation = (mobile) => {
-  return request('/common/mobile-segment/location', false, 'post', { mobile })
-}
-
 module.exports = {
   request,
-  queryMobileLocation
+  queryMobileLocation: (data) => {
+    return request('/common/mobile-segment/location', false, 'get', data)
+  },
+  queryConfig: (data) => {
+    return request('/config/get-value', true, 'get', data)
+  },
+  scoreRules: (data) => {
+    return request('/score/send/rule', true, 'post', data)
+  },
+  kanjiaList: (data) => {
+    return request('/shop/goods/kanjia/list', true, 'post', data)
+  },
+  checkToken: (token) => {
+    return request('/user/check-token', true, 'get', { token })
+  },
+  addTempleMsgFormid: (data) => {
+    return request('/template-msg/wxa/formId', true, 'post', data)
+  },
+  sendTempleMsg: (data) => {
+    return request('/template-msg/put', true, 'post', data)
+  },
+  wxpay: (data) => {
+    return request('/pay/wx/wxapp', true, 'post', data)
+  },
+  login: (code) => {
+    return request('/user/wxapp/login', true, 'post', { code, type:2 })
+  },
+  register: (data) => {
+    return request('/user/wxapp/register/complex', true, 'post', data)
+  }
 }
